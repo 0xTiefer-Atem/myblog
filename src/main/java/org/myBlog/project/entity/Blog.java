f4bbf4c4444e1c204917cc72b020280b4ce84a4d
0xMilk <1144502582@qq.com>
2020-05-27 10:59:36 +0800

    init

---
+++ src/main/java/org/myBlog/project/entity/Blog.java
@@ -0,0 +1,53 @@
+package org.myBlog.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 文章表(Blog)实体类
+ *
+ * @author makejava
+ * @since 2020-05-19 16:03:59
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Blog implements Serializable {
+    private static final long serialVersionUID = 762230905835313737L;
+    
+    private Integer id;
+    /**
+    * 文章id
+    */
+    private String blogId;
+    /**
+    * 文章分类
+    */
+    private String blogType;
+    /**
+    * 文章标签
+    */
+    private String blogTagList;
+
+
+    private List<BlogTag> blogTagListJson;
+    /**
+    * 文章标题
+    */
+    private String blogTitle;
+    /**
+    * 文章内容
+    */
+    private Object blogContent;
+    /**
+    * 文章创建时间
+    */
+    private Date createTime;
+
+}

ca654dfb5ca63a1715e9b49867b67f82547e5b79
0xMilk <1144502582@qq.com>
2020-05-27 10:59:54 +0800

    init

---
+++ src/main/java/org/myBlog/project/util/GetUUID.java
@@ -0,0 +1,12 @@
+package org.myBlog.project.util;
+
+
+import java.util.Date;
+import java.util.UUID;
+
+public class GetUUID {
+    private static Date date = new Date();
+    public static String getUUID(){
+        return UUID.randomUUID().toString().replace("-","").subSequence(1,5).toString()+date.getTime();
+    }
+}

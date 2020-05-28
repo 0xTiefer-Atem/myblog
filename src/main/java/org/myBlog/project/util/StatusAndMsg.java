5f02df1da642e7b7e5820ef3a519971f9886d3b9
0xMilk <1144502582@qq.com>
2020-05-28 09:23:31 +0800

    init

---
+++ src/main/java/org/myBlog/project/util/StatusAndMsg.java
@@ -0,0 +1,12 @@
+package org.myBlog.project.util;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class StatusAndMsg {
+    public static int ERROR1_STATUS =  1001;
+    public static String ERROR1_MSG = "查询错误";
+
+
+    public static int SUCCESS1_STATUS =  2000;
+    public static String SUCCESS1_MSG = "查询成功";
+}

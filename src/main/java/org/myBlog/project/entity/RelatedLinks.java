030d6eef7b72f21aaa3ddac5ffd83018b4424302
0xMilk <1144502582@qq.com>
2020-05-28 09:21:51 +0800

    init

---
+++ src/main/java/org/myBlog/project/entity/RelatedLinks.java
@@ -0,0 +1,14 @@
+package org.myBlog.project.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RelatedLinks {
+    private String webLink;
+    private String webIcon;
+}

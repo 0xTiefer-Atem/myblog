36dbf49bec5bdfecf749b456e04fd31ea0b77713
0xMilk <1144502582@qq.com>
2020-05-29 13:41:58 +0800

    init

---
+++ src/test/java/org/myBlog/project/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myBlog.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}

8419257536d9346685261af5785f1196d5d0bf4a
0xMilk <1144502582@qq.com>
2020-05-29 13:41:45 +0800

    init

---
+++ src/main/java/org/myBlog/project/MyBlogApplication.java
@@ -0,0 +1,14 @@
+package org.myBlog.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.myBlog.project.dao")
+public class MyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogApplication.class, args);
+    }
+}

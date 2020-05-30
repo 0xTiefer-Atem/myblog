86bb0512f89258f8c80f51c8627ca7cffdeb17fe
0xMilk <1144502582@qq.com>
2020-05-30 19:17:39 +0800

    sql里添加查询条件

--- src/main/java/org/myBlog/project/dao/BlogDao.java
+++ src/main/java/org/myBlog/project/dao/BlogDao.java
@@ -22,16 +22,17 @@ public interface BlogDao {
      * @return 实例对象
      */
 
-    @Select("select * from blog where blog_id = #{id}")
+    @Select("select * from blog where blog_id = #{id} and blog_status = #{status}")
     @Results(id = "blogMap",value = {
             @Result(property = "blogId", column = "blog_id"),
             @Result(property = "blogType", column = "blog_type"),
             @Result(property = "blogTagList",column = "blog_tag_list"),
+            @Result(property = "blogOverview",column = "blog_overview"),
             @Result(property = "blogTitle",column = "blog_title"),
             @Result(property = "blogContent",column = "blog_content"),
             @Result(property = "createTime",column = "create_time")
     })
-    Blog queryById(String id);
+    Blog queryById(String id, String status);
 
     /**
      * 查询指定行数据
@@ -41,10 +42,14 @@ public interface BlogDao {
      * @return 对象列表
      */
 
-    @Select("select * from blog limit #{offset}, #{limit}")
+    @Select("select blog_id, blog_type, blog_tag_list, blog_overview, blog_title, create_time  from blog where blog_status = 1 limit #{offset}, #{limit}")
     @ResultMap("blogMap")
     List<Blog> selectAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
 
+
+    @Select("select blog_id, blog_type, blog_tag_list, blog_overview, blog_title, create_time from blog where blog_status = 2")
+    @ResultMap("blogMap")
+    List<Blog> selectSpecialArticle();
 
     /**
      * 通过实体作为筛选条件查询
@@ -55,10 +60,5 @@ public interface BlogDao {
     @Select("select * from blog")
     @ResultMap("blogMap")
     List<Blog> selectAll();
-
-
-
-
-
 
 }

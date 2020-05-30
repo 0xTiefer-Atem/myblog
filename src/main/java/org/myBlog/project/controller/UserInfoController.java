77bbd142c25e169d287ff284a21cbed921457018
0xMilk <1144502582@qq.com>
2020-05-30 19:17:13 +0800

    将用户里的相关链接转换为对象数组形式

--- src/main/java/org/myBlog/project/controller/UserInfoController.java
+++ src/main/java/org/myBlog/project/controller/UserInfoController.java
@@ -1,6 +1,9 @@
 package org.myBlog.project.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.myBlog.project.dao.UserInfoDao;
+import org.myBlog.project.entity.RelatedLinks;
+import org.myBlog.project.entity.SkillInfo;
 import org.myBlog.project.entity.UserInfo;
 import org.myBlog.project.util.ResponseHelper;
 import org.myBlog.project.util.ResponseV2;
@@ -8,6 +11,7 @@ import org.myBlog.project.util.StatusAndMsg;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 个人信息(UserInfo)表控制层
@@ -17,6 +21,7 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping("/api/blog")
+@CrossOrigin
 public class UserInfoController {
     /**
      * 服务对象
@@ -38,6 +43,15 @@ public class UserInfoController {
         }catch (Exception e) {
             return ResponseHelper.create(StatusAndMsg.ERROR1_STATUS,StatusAndMsg.ERROR1_MSG);
         }
+
+        List<RelatedLinks> relatedLinks = JSON.parseArray(userInfo.getUserRelatedLinks(),RelatedLinks.class);
+
+        userInfo.setUserRelatedLinksJson(relatedLinks);
+
+        List<SkillInfo> skillInfos = JSON.parseArray(userInfo.getUserSkillInfoList(),SkillInfo.class);
+
+        userInfo.setUserSkillInfoListJson(skillInfos);
+
         return ResponseHelper.create(userInfo,StatusAndMsg.SUCCESS1_STATUS,StatusAndMsg.SUCCESS1_MSG);
     }
 

8fca1421184bc46530dc7feceaf4f002bd35701a
Wq <1144502582@qq.com>
2020-09-28 23:46:16 +0800

    增加BlogResponse返回VO

---
+++ src/main/java/org/myBlog/project/vo/bolg/response/BlogResponse.java
@@ -0,0 +1,51 @@
+package org.myBlog.project.vo.bolg.response;
+
+/**
+ * @Author WangQian
+ * @Date 2020/9/28 下午 11:36
+ */
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 每篇博客所有信息
+ * */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BlogResponse {
+    /**
+     * 博客id
+     * */
+    private String blogId;
+    /**
+     * 分类
+     * */
+    private String blogType;
+    /**
+     * 相关标签
+     * */
+    private String blogTagList;
+    /**
+     * 标题
+     * */
+    private String blogTitle;
+    /**
+     * 概览
+     * */
+    private String blogOverview;
+    /**
+     * 内容
+     * */
+    private String blogContent;
+    /**
+     * 创建时间
+     * */
+    private Date createTime;
+}

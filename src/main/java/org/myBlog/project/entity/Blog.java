7b6870dc98d1e8b35e50a652f7997ce929a4ea38
Wq <1144502582@qq.com>
2020-09-29 14:21:32 +0800

    增加 Builder 注解

--- src/main/java/org/myBlog/project/entity/Blog.java
+++ src/main/java/org/myBlog/project/entity/Blog.java
@@ -1,6 +1,7 @@
 package org.myBlog.project.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -18,6 +19,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Blog {
 
     private Integer id;

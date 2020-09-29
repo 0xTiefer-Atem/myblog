f1cfe9150d5d83e4fbb2fa7d85e312a0b3b3cc5e
Wq <1144502582@qq.com>
2020-09-29 14:23:04 +0800

    修改 queryBlogList 返回类型,增加 querySpecialBlog 接口

--- src/main/java/org/myBlog/project/service/BlogService.java
+++ src/main/java/org/myBlog/project/service/BlogService.java
@@ -1,5 +1,6 @@
 package org.myBlog.project.service;
 
+import com.github.pagehelper.PageInfo;
 import org.myBlog.project.entity.Blog;
 import org.myBlog.project.vo.bolg.response.BlogInfoResponse;
 import org.myBlog.project.vo.bolg.response.BlogResponse;
@@ -16,9 +17,13 @@ public interface BlogService {
     /**
      * 查询博客列表
      * */
-    List<BlogInfoResponse> queryBlogList(Integer pageNum, Integer pageSize);
+    PageInfo<BlogInfoResponse> queryBlogList(Integer pageNum, Integer pageSize);
     /**
      * 根据blogId查询博客
      * */
     BlogResponse queryBlogByBlogId(String blogId);
+    /**
+     * 查询在校经历与工作经历
+     * */
+    List<BlogResponse> querySpecialBlog();
 }

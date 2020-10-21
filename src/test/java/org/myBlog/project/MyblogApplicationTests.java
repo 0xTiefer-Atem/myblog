7d453b6d0d377bc47e768ff63a879b41c2087575
Wq <1144502582@qq.com>
2020-10-21 19:18:54 +0800

    redis存储测试

--- src/test/java/org/myBlog/project/MyblogApplicationTests.java
+++ src/test/java/org/myBlog/project/MyblogApplicationTests.java
@@ -3,7 +3,9 @@ package org.myBlog.project;
 import org.junit.jupiter.api.Test;
 import org.myBlog.project.entity.Account;
 import org.myBlog.project.mapper.AccountMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
@@ -12,6 +14,16 @@ import javax.annotation.Resource;
 class MyBlogApplicationTests {
     @Resource
     private AccountMapper accountMapper;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void test() {
+        // 保存字符串
+        stringRedisTemplate.opsForValue().set("eee", "111");
+        String o = stringRedisTemplate.opsForValue().get("aaa");
+        System.out.println(o);
+    }
 
     @Test
     void contextLoads() {
